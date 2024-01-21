d3c5a106688858727d8eb2c2595037020200e894
Abir <abirmahmud221@gmail.com>
2024-01-22 00:02:07 +0600

    ...

--- src/Layout/BlogsLayout.jsx
+++ src/Layout/BlogsLayout.jsx
@@ -1,15 +1,22 @@
+import { useLoaderData } from "react-router-dom";
 import BlogsFooter from "../Components/Footer/BlogsFooter";
 import BlogsNavbar from "../Components/Navbar/BlogsNavbar";
+import Blogs from "../Pages/Blogs/Blogs";
 
 
 const BlogsLayout = () => {
 
+    const blogs = useLoaderData();
+    console.log(blogs);
     
 
     return (
         <div className="min-h-screen">
             <BlogsNavbar></BlogsNavbar>
             <BlogsFooter></BlogsFooter>
+            {
+                blogs.map(blog => <Blogs></Blogs>)
+            }
         </div>
     );
 };
