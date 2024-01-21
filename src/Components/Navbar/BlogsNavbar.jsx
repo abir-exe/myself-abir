6ba95c78090d14d0b3ce39a05067fc45f24c82f2
Abir <abirmahmud221@gmail.com>
2024-01-21 23:46:43 +0600

    ..

--- src/Components/Navbar/BlogsNavbar.jsx
+++ src/Components/Navbar/BlogsNavbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 
 const BlogsNavbar = () => {
@@ -9,7 +10,7 @@ const BlogsNavbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Homepage</a></li>
+        <li><Link to="/">Homepage</Link></li>
         <li><a>Portfolio</a></li>
         <li><a>About</a></li>
       </ul>
